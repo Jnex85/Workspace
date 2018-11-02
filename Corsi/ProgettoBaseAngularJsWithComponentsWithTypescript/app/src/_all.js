50d2888a48e7e127001e8b36eb4b18418b609e20
Josef <giuseppe.neutro@outlook.com>
2018-11-02 16:46:57 +0100

    modified typescript compiler config

--- Corsi/ProgettoBaseAngularJsWithComponentsWithTypescript/app/src/_all.js
+++
@@ -1,5 +0,0 @@
-/// <reference path="../../node_modules/@types/angular/index.d.ts" />
-/// <reference path="../../node_modules/@types/angular/jqlite.d.ts" />
-/// <reference path="../../node_modules/@types/angular-material/index.d.ts" />
-/// <reference path="../../node_modules/@types/lodash/index.d.ts" />
-/// <reference path="../../node_modules/@types/angular-route/index.d.ts" />
